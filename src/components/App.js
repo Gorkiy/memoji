061270a03f6dd022b3131798e9aa030f3b2a3027
Peter Yakovlev <gorkiy@gmail.com>
2019-07-24 17:01:03 +0300

    Render basic game field mockup

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import Card from './Card';
 
 class App extends Component {
+  state = { 
+    gameStarted: false,
+    fieldSize: 12 
+  };
+  
+  renderGameField(size) {
+    let template = [];
+    for (let i = 0; i < size; i++) {
+      template.push(<Card key={i}/>);
+    }
+    return template;
+  }
 
   render() {
     return (
-      <div>
-        My App
+      <div className="game-field">
+        {this.renderGameField(this.state.fieldSize)}
       </div>
     );
   }
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+
+  render() {
+    return (
+      <div className="game-field__card"></div>
+    );
+  }
+}
+
+export default Card;
