92b6414e8849fa53dca24ccc9eb468d5bafc6e0a
Peter Yakovlev <gorkiy@gmail.com>
2019-07-26 12:18:14 +0300

    Complete card pairs flipping logic

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,97 @@ import React, { Component } from 'react';
 import Card from './Card';
 import { shuffle } from 'lodash';
 
-const emojis = ['🐶', '🐶', '🐱', '🐱', '🐭', '🐭', '🐹', '🐹', '🐰', '🐰', '🐻', '🐻'];
+const emojis = shuffle(['🐶', '🐶', '🐱', '🐱', '🐭', '🐭', '🐹', '🐹', '🐰', '🐰', '🐻', '🐻']);
+// const emojis = shuffle(['🐶', '🐶', '🐱', '🐱']);
 
 class App extends Component {
   state = { 
     gameStarted: false,
-    fieldSize: 12,
-    cards: shuffle(emojis)
+    cardsLeft: 12,
+    cardsData: {},
+    cards: [],
+    exposedCount: 0,
+    exposedCards: []
   };
   
-  renderGameField(size) {
-    let template = [];
-    for (let i = 0; i < size; i++) {
-      template.push(<Card key={i} id={i} cardFace={this.state.cards[i]}/>);
+  componentDidMount() {
+    const data = this.getCardsData(emojis);
+    this.setState({ cardsData: data });
+    this.setState( { cards: this.getCardComponents(data) });
+  }
+  
+  getCardsData(emojis) {
+    let data = {};
+    for (let i = 0; i < emojis.length; i++) {
+      data[i] = {
+        id: i,
+        cardFace: emojis[i],
+        isMatched: false,
+        exposed: false
+      }
     }
-    console.log(template);
-    return template;
+    return data;
+  }
+  
+  getCardComponents(data) {
+    let cards = [];
+    for (let key in data) {
+      let card = data[key];
+      cards.push(<Card 
+        key={card.id} 
+        id={card.id} 
+        cardFace={card.cardFace} 
+        isMatched={card.isMatched} 
+        exposed={card.exposed} 
+        onClick={this.onCardClick}
+      />);
+    }
+    return cards;
+  }
+  
+  markAsMatched(id1, id2) {
+    const data = this.state.cardsData;
+    data[id1]['isMatched'] = true;
+    data[id2]['isMatched'] = true;
+    this.update(data);
+  }
+  
+  switchExposed(id1, id2) {
+    const data = this.state.cardsData;
+    data[id1]['exposed'] = !data[id1]['exposed'];
+    data[id2]['exposed'] = !data[id2]['exposed'];
+    this.update(data);
+  }
+  
+  async update (data) {
+    const newData = await this.getCardComponents(data);
+    this.setState( { cards: newData });
+  }
+  
+  onCardClick = async (card) => {
+    this.state.exposedCards.push(card);
+    await this.setState({ exposedCount: this.state.exposedCount + 1 });
+    
+    if (this.state.exposedCount === 2) {
+      let first = this.state.exposedCards[0];
+      let second = this.state.exposedCards[1];
+      if (first.props.cardFace === second.props.cardFace)  {
+        this.markAsMatched(first.props.id, second.props.id);
+      } else {
+        this.switchExposed(first.props.id, second.props.id);
+        // Uncheck cards as exposed in a second
+        setTimeout(function() {
+          this.switchExposed(first.props.id, second.props.id);
+        }.bind(this), 1000);
+      }
+      this.setState({ exposedCount: 0, exposedCards: [] });
+    }
   }
 
   render() {
     return (
       <div className="game-field">
-        {this.renderGameField(this.state.fieldSize)}
+        {this.state.cards}
       </div>
     );
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -8,24 +8,52 @@ class Card extends Component {
     super(props);
     this.state = {
       isFlipped: false,
-      isMatched: false
+      exposed: false
     }
     this.handleClick = this.handleClick.bind(this);
   }
   
-  handleClick(e) {
+  componentDidUpdate(prevProps) {
+    if (prevProps.exposed !== this.props.exposed && this.props.exposed === true ) {
+      this.setState( {exposed: true} );
+    }
+    
+    if (this.state.exposed) {
+      this.flip();
+      this.setState( {exposed: false} );
+    }
+  }
+  
+  flip() {
+    setTimeout(function() {
+      this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+    }.bind(this), 1000);
+  }
+  
+  handleClick() {
     if (!this.state.isFlipped) {
       this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+      this.props.onClick(this);
+    }
+  }
+  
+  renderCard() {
+    if (this.state.isMatched) {
+      return <CardFront cardFace={this.props.cardFace} />   
+    } else {
+      return (
+          <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+            <CardFront key="back" onClick={this.handleClick} cardFace={this.props.cardFace} />   
+            <CardBack key="front" onClick={this.handleClick} />
+          </ReactCardFlip>
+          )
     }
   }
 
   render() {
     return (
       <div className="game-field__tile">
-        <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
-          <CardFront key="back" onClick={this.handleClick} cardFace={this.props.cardFace} />   
-          <CardBack key="front" onClick={this.handleClick} />
-        </ReactCardFlip>
+        { this.renderCard() }
       </div>
     );
   }
