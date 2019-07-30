17b0948c9e32243b2ba0d31d350f02bd34b05e88
Peter Yakovlev <gorkiy@gmail.com>
2019-07-30 16:47:39 +0300

    Updates

---
+++ precache-manifest.59cfa630ee895a41793a85811cfc48f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885e1edc2f8b1cbdac08802a7a081eae",
+    "url": "/memoji2/index.html"
+  },
+  {
+    "revision": "e7e9f1fdfc4286bbb61c",
+    "url": "/memoji2/static/css/main.c0837efc.chunk.css"
+  },
+  {
+    "revision": "476e71f01dba2f3e167f",
+    "url": "/memoji2/static/js/2.3f0692bb.chunk.js"
+  },
+  {
+    "revision": "e7e9f1fdfc4286bbb61c",
+    "url": "/memoji2/static/js/main.31ed6395.chunk.js"
+  },
+  {
+    "revision": "d0f6f3b71eb52c854136",
+    "url": "/memoji2/static/js/runtime~main.be4d794d.js"
+  }
+]);
